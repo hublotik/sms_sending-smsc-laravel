f9bda703817ac9e8d0e8c602728c312859a85d7c
petr <golubev-petya@mail.ru>
2024-02-17 14:12:23 +0400

    migrations user fix + define kernel schedule logic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,23 +4,41 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use App\Models\User;
+use Khodja\Smsc\Smsc; //smsc extension
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
      */
+    public $sms_text;
+    public function __construct($sms_text = '') {
+        $this->sms_text = $sms_text;
+    }
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $users = User::all();
+            foreach ($users as $user) {
+                $currentYear = date('Y');
+                $birth_date_with_curr_year = date('Y-m-d', strtotime("$currentYear-" . date('m-d', strtotime($user->birth_date))));
+                $daysLeft = now()->diffInDays($birth_date_with_curr_year);
+                if ($daysLeft == 7) {
+                    // Perform the action to schedule the event if 7 days left to date
+                    SmsC::send($user->phone_number, $this->sms_text);
+                }
+            }
+        })->dailyAt('10:30');
     }
+
 
     /**
      * Register the commands for the application.
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- resources/views/template/partials/sidebar.blade.php
+++ resources/views/template/partials/sidebar.blade.php
@@ -21,7 +21,7 @@
         <ul class="sidebar-menu scrollable pos-r">
             <li class="nav-item mT-30 actived"><a class="sidebar-link" href="/"><span class="icon-holder"><i class="c-blue-500 ti-home"></i> </span><span class="title">Dashboard</span></a></li>
            
-            <li class="nav-item"><a class="sidebar-link" href="/chat"><span class="icon-holder"><i class="c-deep-purple-500 ti-comment-alt"></i> </span><span class="title">Chat</span></a></li>
+            <li class="nav-item"><a class="sidebar-link" href="/chat"><span class="icon-holder"><i class="c-deep-purple-500 ti-comment-alt"></i> </span><span class="title">Статистика</span></a></li>
           
             </li>
     
