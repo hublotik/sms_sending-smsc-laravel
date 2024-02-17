a3d7db33b6c338f7717b588b5fe8d475302d783a
golubev-petya@outlook.com <golubev-petya@outlook.com>
2024-02-17 23:14:24 +0400

    fix schedule function name + better representation with exteranal trait

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,88 +4,24 @@ namespace App\Console;
 
 use Illuminate\Support\Facades\Log;
 use Exception;
-
+use App\ScheduleTrait;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Models\User;
 use App\Models\ScheduleEvents;
-use App\Models\SMS;
-use Khodja\Smsc\Smsc; //smsc extension
 
 class Kernel extends ConsoleKernel
 {
+    use ScheduleTrait;
     /**
      * Define the application's command schedule.
      */
 
-    //send sms to users
-    protected function schedule_sms_send(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-        $sch_events = ScheduleEvents::all();
-        $users = User::all();
-        foreach ($sch_events as $event) {
-            $schedule->call(function () use ($event, $users) {
-                $userIds = [];
-                foreach ($users as $user) {
-                    try {
-                        $currentYear = date('Y');
-                        $birth_date_with_curr_year = date('Y-m-d', strtotime("$currentYear-" . date('m-d', strtotime($user->birth_date))));
-                        $daysLeft = now()->diffInDays($birth_date_with_curr_year);
-                        if ($daysLeft == $event->time_offset) {
-                            //send sms right on time ;)
-                            SmsC::send($user->phone_number, $event->sms_text, 0, 0, $event->id);
-                            $userIds[] = $user->id;
-                        }
-                    } catch (\Exception $e) {
-                        Log::error('An error occurred: ' . $e->getMessage());
-                    }
-                }
-                $serializedUserIds = serialize($userIds);
-                $sms_stat = SMS::firstOrCreate([
-                    'id' => $event->id,
-                    'sms_name' => $event->event_name,
-                    'serialized_users_ids' => $serializedUserIds,
-                    'completion' => 0,
-                ]);
-            })->dailyAt($event->send_time);
-        }
-    }
-
-    //completion upd each 10 minutes
-    protected function event_status_update(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-        $sended_sms_events = SMS::all();
-        foreach ($sended_sms_events as $event) {
-            $users_in_event = unserialize($event->serialized_users_ids);
-            $schedule->call(function () use ($event, $users_in_event) {
-                foreach ($users_in_event as $user_id) {
-                    $phone_numbers = User::where('id', $user_id)->select('phone_number')->get();
-                    //iterate through numbers for checking status of each number
-                    $pos_res = 0;
-                    foreach ($phone_numbers as $phone_number) {
-                        try {
-                            $curr_num_stat = Smsc::getStatus($event->id, $phone_number);
-
-                            // (status, time, err, ...) или (0, -error)
-                            if (count($curr_num_stat) > 2) {
-                                //check that we actually do not have an error
-                                $pos_res++;
-                            }
-                        } catch (Exception $e) {
-                            Log::error('An error occurred: ' . $e->getMessage());
-                        }
-                    }
-                    $completion = ($pos_res / count($phone_numbers)) * 100;
-                }
-
-                SMS::where('id', $event->id)->update([
-                    'completion' => $completion,
-                ]);
-            })->everyTenMinutes(); // for example every 10 mins
-        }
-    }
+     //send sms to users
+     protected function schedule(Schedule $schedule): void
+     {
+        self::schedule_sms_send($schedule);
+        self::event_status_update($schedule);
+     }
 
 
     /**
---
+++ app/ScheduleTrait.php
@@ -0,0 +1,82 @@
+<?php
+namespace App;
+
+use Illuminate\Console\Scheduling\Schedule;
+use App\Models\ScheduleEvents;
+use App\Models\User;
+use Khodja\Smsc\Smsc; //Smsc extension
+use App\Models\SMS;
+
+trait ScheduleTrait
+{
+    protected static function schedule_sms_send(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->hourly();
+        $sch_events = ScheduleEvents::all();
+        $users = User::all();
+
+        foreach ($sch_events as $event) {
+            $schedule->call(function () use ($event, $users) {
+                $userIds = [];
+                foreach ($users as $user) {
+                    try {
+                        $currentYear = date('Y');
+                        $birth_date_with_curr_year = date('Y-m-d', strtotime("$currentYear-" . date('m-d', strtotime($user->birth_date))));
+                        $daysLeft = now()->diffInDays($birth_date_with_curr_year);
+
+                        if ($daysLeft == $event->time_offset) {
+                            //send sms right on time ;)
+                            SmsC::send($user->phone_number, $event->sms_text, 0, 0, $event->id);
+                            $userIds[] = $user->id;
+                        }
+                    } catch (\Exception $e) {
+
+                        Log::error('An error occurred: ' . $e->getMessage());
+                    }
+                }
+                $serializedUserIds = serialize($userIds);
+
+                $sms_stat = SMS::firstOrCreate([
+                    'id' => $event->id,
+                    'sms_name' => $event->event_name,
+                    'serialized_users_ids' => $serializedUserIds,
+                    'completion' => 0,
+                ]);
+            })->dailyAt($event->send_time);
+        }
+    }
+
+    protected static function event_status_update(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->hourly();
+        $sended_sms_events = SMS::all();
+        foreach ($sended_sms_events as $event) {
+            $users_in_event = unserialize($event->serialized_users_ids);
+            $schedule->call(function () use ($event, $users_in_event) {
+                foreach ($users_in_event as $user_id) {
+                    $phone_numbers = User::where('id', $user_id)->select('phone_number')->get();
+                    //iterate through numbers for checking status of each number
+                    $pos_res = 0;
+                    foreach ($phone_numbers as $phone_number) {
+                        try {
+                            $curr_num_stat = Smsc::getStatus($event->id, $phone_number);
+
+                            // (status, time, err, ...) или (0, -error)
+                            if (count($curr_num_stat) > 2) {
+                                //check that we actually do not have an error
+                                $pos_res++;
+                            }
+                        } catch (Exception $e) {
+                            Log::error('An error occurred: ' . $e->getMessage());
+                        }
+                    }
+                    $completion = ($pos_res / count($phone_numbers)) * 100;
+                }
+
+                SMS::where('id', $event->id)->update([
+                    'completion' => $completion,
+                ]);
+            })->everyTenMinutes(); // for example every 10 mins
+        }
+    }
+}
